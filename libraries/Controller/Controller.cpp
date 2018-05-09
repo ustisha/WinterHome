e896f0923c883ff9b6531849e1a195c08c7c899c
Dmitriy Deryabin <dmitriy@ustisha.ru>
2018-05-09 13:24:57 +0700

    Задержка при инициализации LoRa модуля.

--- libraries/Controller/Controller.cpp
+++ libraries/Controller/Controller.cpp
@@ -2,8 +2,15 @@
 #include "Controller.h"
 
 Controller::Controller(uint8_t cs, uint8_t dc, uint8_t reset) {
+    // Задержка перед инициализацией.
+    delay(1000);
+
     LoRa.begin(433E6);
     LoRa.setTxPower(17);
     LoRa.setSpreadingFactor(12);
     LoRa.enableCrc();
+    LoRa.idle();
+
+    // Задержка после инициализации.
+    delay(1000);
 }
